138ec0ee7de8c8e602bf75cd0848ffc92aca3459
Dan <peritract@hotmail.co.uk>
2023-11-21 09:40:53 +0000

    Add files

---
+++ README.d
@@ -0,0 +1,26 @@
+# Word Game (Scrabble)
+
+## Background
+
+The [Caesar Cipher](https://en.wikipedia.org/wiki/Caesar_cipher) is an encryption technique which encodes information by shifting each letter a certain number of places in the alphabet.
+
+For example, using a shift of `2`, the string "hello" is encoded as "jgnnq". "elephant" is "ovozrkxd" using a shift of 10.
+
+In this challenge, you'll write code to encrypt and decrypt information using the Caesar Cipher.
+
+## ⚠️ Note
+
+There is **far** too much work here to be done in the time you've been given. You are **not** expected to complete the whole challenge. 
+
+## Tasks
+
+1. Decrypt an encrypted string using a shift of 13.
+2. Decrypt an encrypted string using a given shift.
+3. Encode a plaintext string using a shift of 10. Only letters (A-Z, a-z) should be transformed.
+4. Encode a plaintext string using a provided shift of 10. Only letters (A-Z, a-z) should be transformed.
+5. Given an encrypted string with an unknown shift, identify all possible decryptions.
+6. Given an encrypted string with an unknown shift, identify the three most likely decryptions.
+
+## Test data
+
+If you need to generate test data, you can use [this website](https://cryptii.com/pipes/caesar-cipher).
